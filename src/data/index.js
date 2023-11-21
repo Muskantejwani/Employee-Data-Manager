f55c9dad11c052dfd37673034c7c3cde333c2f82
Muskan Tejwani <muskantejwani445@gmail.com>
2023-11-21 20:31:55 +0500

    react js project

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Alice',
+    lastName: 'Smith',
+    email: 'alice.smith@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Robert',
+    lastName: 'Jones',
+    email: 'robert.jones@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Emily',
+    lastName: 'Brown',
+    email: 'emily.brown@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Daniel',
+    lastName: 'Taylor',
+    email: 'daniel.taylor@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Sophia',
+    lastName: 'Miller',
+    email: 'sophia.miller@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Michael',
+    lastName: 'Clark',
+    email: 'michael.clark@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'Ella',
+    lastName: 'Anderson',
+    email: 'ella.anderson@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'David',
+    lastName: 'Williams',
+    email: 'david.williams@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Olivia',
+    lastName: 'Johnson',
+    email: 'olivia.johnson@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
